546764e421172bcf7b68f4f2de8affa304244aa4
YongminKwon <yongmin.kwon@adler.cx>
2022-01-18 21:13:24 +0900

    michinjango

---
+++ main/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('blog/', views.blog, name='blog'),
+    path('blog/<int:pk>/', views.posting, name="posting"),
+    path('blog/new_post/', views.new_post),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
