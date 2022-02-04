569d70e3d67e78af8b657e12e1a418fb7ee24311
YongminKwon <yongmin.kwon@adler.cx>
2022-02-04 14:43:36 +0900

    edit admin page

--- main/admin.py
+++ main/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 from .models import Post
 
 # Register your models here.
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Title', {'fields' : ['postname']}), 
+        ('Contents', {'fields' : ['contents']}),
+        ('Photo', {'fields' : ['mainphoto'], 'classes': ['collapse']}),
+    ]
+
+admin.site.register(Post, PostAdmin)
+
--- main/models.py
+++ main/models.py
@@ -5,6 +5,7 @@ class Post(models.Model):
     postname = models.CharField(max_length = 50)
     mainphoto = models.ImageField(blank=True, null=True)
     contents = models.TextField()
+    # model.DateTimeField('date published') 도 가능
 
     def __str__(self):
         return self.postname
